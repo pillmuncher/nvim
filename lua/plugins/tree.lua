d1afd8d99c66f32a6425947e19491f1e17f57b40
Mick Krippendorf <m.krippendorf@freenet.de>
2023-10-12 18:53:38 +0200

    fixed bug where keymaps weren't done.

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -9,16 +9,18 @@ return {
         { "nvim-tree/nvim-web-devicons", opts = {} },
     },
     opts = {},
-    {
-        "<C-e>",
-        "<CMD> NvimTreeToggle <CR>",
-        desc = "Toggle nvimtree",
-        mode = { "n", "i" }
-    },
-    {
-        "<leader>e",
-        "<CMD> NvimTreeFocus <CR>",
-        desc = "Focus nvimtree",
-        mode = { "n" }
-    },
+    keys = {
+        {
+            "<C-e>",
+            "<CMD> NvimTreeToggle <CR>",
+            desc = "Toggle nvimtree",
+            mode = { "n", "i" }
+        },
+        {
+            "<leader>e",
+            "<CMD> NvimTreeFocus <CR>",
+            desc = "Focus nvimtree",
+            mode = { "n" }
+        },
+    }
 }
