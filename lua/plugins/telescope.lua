2eda690a866103b89aa3a81957135e825a848ecd
Mick Krippendorf <m.krippendorf@freenet.de>
2023-10-17 07:14:32 +0200

    removed Themer

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -88,15 +88,5 @@ return {
             desc = 'Find bookmarks',
             mode = { 'n' },
         },
-        {
-            '<leader>ff',
-            function()
-                -- You can pass additional configuration to telescope to change theme, layout, etc.
-                require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-                    winblend = 10,
-                    previewer = false,
-                })
-            end,
-            desc = 'Find Fuzzily in current buffer',
-        },    },
+    },
 }
--- lua/settings/plugins/telescope.lua
+++ lua/settings/plugins/telescope.lua
@@ -1,5 +1,2 @@
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
-
--- Enable telescope Themer support, if installed
-pcall(require('telescope').load_extension, 'themes')
