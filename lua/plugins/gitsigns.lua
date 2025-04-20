b5e3b36df07fa75d382e693e2f0c34d4bfb115a8
Mick Krippendorf <m.krippendorf@freenet.de>
2025-04-20 07:21:59 +0200

    Added nvim-coverage and made it work.

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -1,6 +1,6 @@
 return {
     'lewis6991/gitsigns.nvim',
-    lazy = false,
+    -- lazy = false,
     opts = {
         -- See `:help gitsigns.txt`
         signs = {
--- lua/plugins/neotest.lua
+++ lua/plugins/neotest.lua
@@ -1,22 +1,41 @@
 return {
     'nvim-neotest/neotest',
     dependencies = {
-        { 'nvim-neotest/neotest-python' },
-        { 'nvim-neotest/neotest-plenary' },
-        { 'nvim-neotest/neotest-vim-test' },
-        { 'nvim-neotest/nvim-nio' },
+        'nvim-neotest/neotest-python',
+        'nvim-neotest/neotest-plenary',
+        'nvim-neotest/neotest-vim-test',
+        'andythigpen/nvim-coverage',
+        'nvim-neotest/nvim-nio',
     },
     config = function()
         require('neotest').setup({
             adapters = {
+                require('neotest-plenary'),
                 require('neotest-python')({
                     dap = { justMyCode = false },
+                    python = function()
+                        return vim.fn.system("pdm info --python-path"):gsub("%s+$", "")
+                    end,
                 }),
-                require('neotest-plenary'),
                 require('neotest-vim-test')({
                     ignore_file_types = { 'python', 'vim', 'lua' },
                 }),
             },
+            quickfix = {
+                enabled = true,
+                open = false,
+            },
+            output = {
+                enabled = true,
+                open_on_run = true,
+            },
+            diagnostic = {
+                enabled = true
+            },
+            signs = {
+                enabled = true,
+                priority = 40,
+            }
         })
     end,
 }
