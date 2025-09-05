c709940b375c6858b212dcdb87dbf5ab24cd9bea
Mick Krippendorf <m.krippendorf@freenet.de>
2025-09-05 02:39:18 +0200

    fixed missing last char on 'yaf'

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,110 +1,117 @@
 return {
-    'nvim-treesitter/nvim-treesitter',
-    dependencies = {
-        { 'nvim-treesitter/nvim-treesitter-textobjects' },
-        { 'nvim-treesitter/playground' },
-    },
-    build        = ':TSUpdate',
-    config       = function()
-        vim.defer_fn(function()
-            require('nvim-treesitter.configs').setup({
-                -- Add languages to be installed here that you want installed for treesitter
-                ensure_installed = {
-                    'bash',
-                    'c',
-                    'c_sharp',
-                    'lua',
-                    'markdown',
-                    'python',
-                    'vim',
-                    'vimdoc',
-                },
+	"nvim-treesitter/nvim-treesitter",
+	dependencies = {
+		{ "nvim-treesitter/nvim-treesitter-textobjects" },
+		{ "nvim-treesitter/playground" },
+	},
+	build = ":TSUpdate",
+	config = function()
+		vim.defer_fn(function()
+			require("nvim-treesitter.configs").setup({
+				-- Add languages to be installed here that you want installed for treesitter
+				ensure_installed = {
+					"bash",
+					"c",
+					"c_sharp",
+					"lua",
+					"markdown",
+					"python",
+					"vim",
+					"vimdoc",
+				},
 
-                -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
-                auto_install = false,
+				-- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+				auto_install = false,
 
-                highlight = { enable = true },
-                indent = { enable = true },
-                incremental_selection = {
-                    enable = true,
-                    keymaps = {
-                        init_selection = '<C-Space>',
-                        node_incremental = '<C-Space>',
-                        scope_incremental = '<C-S>',
-                        node_decremental = '<M-Space>',
-                    },
-                },
-                textobjects = {
-                    select = {
-                        enable = true,
-                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
-                        keymaps = {
-                            -- You can use the capture groups defined in textobjects.scm
-                            ['aa'] = '@parameter.outer',
-                            ['ia'] = '@parameter.inner',
-                            ['af'] = '@function.outer',
-                            ['if'] = '@function.inner',
-                            ['ac'] = '@class.outer',
-                            ['ic'] = '@class.inner',
-                        },
-                    },
-                    move = {
-                        enable = true,
-                        set_jumps = true, -- whether to set jumps in the jumplist
-                        goto_next_start = {
-                            [']m'] = '@function.outer',
-                            [']]'] = '@class.outer',
-                        },
-                        goto_next_end = {
-                            [']M'] = '@function.outer',
-                            [']['] = '@class.outer',
-                        },
-                        goto_previous_start = {
-                            ['[m'] = '@function.outer',
-                            ['[['] = '@class.outer',
-                        },
-                        goto_previous_end = {
-                            ['[M'] = '@function.outer',
-                            ['[]'] = '@class.outer',
-                        },
-                    },
-                    swap = {
-                        enable = true,
-                        swap_next = {
-                            ['<leader>a'] = '@parameter.inner',
-                        },
-                        swap_previous = {
-                            ['<leader>A'] = '@parameter.inner',
-                        },
-                    },
-                },
-                playground = {
-                    enable = true,
-                    disable = {},
-                    updatetime = 25,         -- Debounced time for highlighting nodes in the playground from source code
-                    persist_queries = false, -- Whether the query persists across vim sessions
-                    keybindings = {
-                        toggle_query_editor = 'o',
-                        toggle_hl_groups = 'i',
-                        toggle_injected_languages = 't',
-                        toggle_anonymous_nodes = 'a',
-                        toggle_language_display = 'I',
-                        focus_language = 'f',
-                        unfocus_language = 'F',
-                        update = 'R',
-                        goto_node = '<cr>',
-                        show_help = '?',
-                    },
-                    query_linter = {
-                        enable = true,
-                        use_virtual_text = true,
-                        lint_events = { 'BufWrite', 'CursorHold' },
-                    },
-                },
-                modules = {},
-                ignore_install = {},
-                sync_install = false,
-            })
-        end, 0)
-    end,
+				highlight = { enable = true },
+				indent = { enable = true },
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						init_selection = "<C-Space>",
+						node_incremental = "<C-Space>",
+						scope_incremental = "<C-S>",
+						node_decremental = "<M-Space>",
+					},
+				},
+				textobjects = {
+					select = {
+						enable = true,
+						lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+						-- force outer function selection to be linewise
+						selection_modes = {
+							["@function.outer"] = "V", -- linewise
+							["@function.inner"] = "V", -- linewise
+						},
+						keymaps = {
+							-- You can use the capture groups defined in textobjects.scm
+							["aa"] = "@parameter.outer",
+							["ia"] = "@parameter.inner",
+							["af"] = "@function.outer",
+							["if"] = "@function.inner",
+							["ac"] = "@class.outer",
+							["ic"] = "@class.inner",
+						},
+						-- include trailing/leading whitespace (so trailing newline is included)
+						-- include_surrounding_whitespace = true,
+					},
+					move = {
+						enable = true,
+						set_jumps = true, -- whether to set jumps in the jumplist
+						goto_next_start = {
+							["]m"] = "@function.outer",
+							["]]"] = "@class.outer",
+						},
+						goto_next_end = {
+							["]M"] = "@function.outer",
+							["]["] = "@class.outer",
+						},
+						goto_previous_start = {
+							["[m"] = "@function.outer",
+							["[["] = "@class.outer",
+						},
+						goto_previous_end = {
+							["[M"] = "@function.outer",
+							["[]"] = "@class.outer",
+						},
+					},
+					swap = {
+						enable = true,
+						swap_next = {
+							["<leader>a"] = "@parameter.inner",
+						},
+						swap_previous = {
+							["<leader>A"] = "@parameter.inner",
+						},
+					},
+				},
+				playground = {
+					enable = true,
+					disable = {},
+					updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+					persist_queries = false, -- Whether the query persists across vim sessions
+					keybindings = {
+						toggle_query_editor = "o",
+						toggle_hl_groups = "i",
+						toggle_injected_languages = "t",
+						toggle_anonymous_nodes = "a",
+						toggle_language_display = "I",
+						focus_language = "f",
+						unfocus_language = "F",
+						update = "R",
+						goto_node = "<cr>",
+						show_help = "?",
+					},
+					query_linter = {
+						enable = true,
+						use_virtual_text = true,
+						lint_events = { "BufWrite", "CursorHold" },
+					},
+				},
+				modules = {},
+				ignore_install = {},
+				sync_install = false,
+			})
+		end, 0)
+	end,
 }
