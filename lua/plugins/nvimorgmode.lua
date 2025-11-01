f445b570cb657a40ff6992b09ffa076cf254be90
Mick Krippendorf <m.krippendorf@freenet.de>
2025-11-01 20:28:50 +0100

    changed orgmode folders location

--- lua/plugins/nvimorgmode.lua
+++ lua/plugins/nvimorgmode.lua
@@ -5,15 +5,15 @@ return {
 	config = function()
 		-- Setup orgmode
 		require("orgmode").setup({
-			org_agenda_files = "~/orgfiles/**/*",
-			org_default_notes_file = "~/orgfiles/refile.org",
+			org_agenda_files = "~/Documents/org/**/*",
+			org_default_notes_file = "~/Documents/org/refile.org",
 		})
 
 		-- NOTE: If you are using nvim-treesitter with ~ensure_installed = "all"~ option
 		-- add ~org~ to ignore_install
-		-- require('nvim-treesitter.configs').setup({
-		--   ensure_installed = 'all',
-		--   ignore_install = { 'org' },
+		-- require("nvim-treesitter.configs").setup({
+		-- 	ensure_installed = "all",
+		-- 	ignore_install = { "org" },
 		-- })
 	end,
 }
