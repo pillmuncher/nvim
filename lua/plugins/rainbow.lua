224a0d8275c2fe430695332ff384505fb35210e8
Mick Krippendorf <m.krippendorf@freenet.de>
2025-11-03 17:53:55 +0100

    something with rainbow parens

--- lua/plugins/rainbow.lua
+++ lua/plugins/rainbow.lua
@@ -1,34 +1,34 @@
 return {
-    'HiPhish/rainbow-delimiters.nvim',
-    config = function()
-        require('rainbow-delimiters.setup').setup({
-            strategy = {
-                [''] = 'rainbow-delimiters.strategy.global',
-                vim = 'rainbow-delimiters.strategy.local',
-                -- clojure = 'rainbow-delimiters.strategy.local',
-                python = 'rainbow-delimiters.strategy.global',
-            },
-            query = {
-                [''] = 'rainbow-delimiters',
-                lua = 'rainbow-blocks',
-                -- clojure = 'rainbow-delimiters',
-                python = 'rainbow-delimiters',
-            },
-            priority = {
-                [''] = 110,
-                lua = 210,
-            },
-            highlight = {
-                'RainbowDelimiterRed',
-                'RainbowDelimiterYellow',
-                'RainbowDelimiterBlue',
-                'RainbowDelimiterOrange',
-                'RainbowDelimiterGreen',
-                'RainbowDelimiterViolet',
-                'RainbowDelimiterCyan',
-            }
-        })
-    end,
-    -- ft = { 'python', 'lua', 'clojure', 'vim' } -- Ensure it's loaded for Python
-    ft = { 'python', 'lua', 'vim' } -- Ensure it's loaded for Python
+	"HiPhish/rainbow-delimiters.nvim",
+	config = function()
+		require("rainbow-delimiters.setup").setup({
+			strategy = {
+				[""] = "rainbow-delimiters.strategy.global",
+				vim = "rainbow-delimiters.strategy.local",
+				-- clojure = 'rainbow-delimiters.strategy.local',
+				python = "rainbow-delimiters.strategy.global",
+			},
+			query = {
+				[""] = "rainbow-delimiters",
+				lua = "rainbow-blocks",
+				-- clojure = 'rainbow-delimiters',
+				python = "rainbow-delimiters",
+			},
+			priority = {
+				[""] = 110,
+				lua = 210,
+			},
+			highlight = {
+				"RainbowDelimiterRed",
+				"RainbowDelimiterYellow",
+				"RainbowDelimiterBlue",
+				"RainbowDelimiterOrange",
+				"RainbowDelimiterGreen",
+				"RainbowDelimiterViolet",
+				"RainbowDelimiterCyan",
+			},
+		})
+	end,
+	-- ft = { 'python', 'lua', 'clojure', 'vim' } -- Ensure it's loaded for Python
+	ft = { "python", "lua", "vim" }, -- Ensure it's loaded for Python
 }
