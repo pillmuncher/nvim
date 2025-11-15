1276c13940ca1fdb1dd18da808cf95d39d7d70e0
Mick Krippendorf <m.krippendorf@freenet.de>
2025-11-16 00:22:45 +0100

    added cmp-conjure

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,95 +1,94 @@
 local has_words_before = function()
-    local unpack = unpack or table.unpack
-    local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-    return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match('%s') == nil
+	local unpack = unpack or table.unpack
+	local line, col = unpack(vim.api.nvim_win_get_cursor(0))
+	return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
 end
 
 return {
-    'hrsh7th/nvim-cmp',
-    lazy         = false,
-    dependencies = {
-        -- { "Olical/conjure" },
-        -- { 'PaterJason/cmp-conjure' },
-        { 'L3MON4D3/LuaSnip',            build = "make install_jsregexp" },
-        { 'hrsh7th/cmp-nvim-lsp' },
-        { 'rafamadriz/friendly-snippets' },
-        { 'saadparwaiz1/cmp_luasnip' },
-    },
-    opts         = function(_, opts)
-        if type(opts.sources) == "table" then
-            return
-            -- vim.list_extend(opts.sources, { name = "conjure" })
-        end
-    end,
-    config       = function()
-        local cmp = require('cmp')
-        local luasnip = require('luasnip')
-        require('luasnip.loaders.from_vscode').lazy_load()
+	"hrsh7th/nvim-cmp",
+	lazy = false,
+	dependencies = {
+		{ "Olical/conjure" },
+		{ "L3MON4D3/LuaSnip", build = "make install_jsregexp" },
+		{ "hrsh7th/cmp-nvim-lsp" },
+		{ "rafamadriz/friendly-snippets" },
+		{ "saadparwaiz1/cmp_luasnip" },
+	},
+	opts = function(_, opts)
+		if type(opts.sources) == "table" then
+			return
+			-- vim.list_extend(opts.sources, { name = "conjure" })
+		end
+	end,
+	config = function()
+		local cmp = require("cmp")
+		local luasnip = require("luasnip")
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-        cmp.setup({
-            sources = cmp.config.sources({
-                -- { name = 'clojure' },
-                -- { name = 'conjure' },
-                { name = 'buffer' },
-                { name = 'luasnip' },
-                { name = 'nvim_lsp' },
-                { name = 'nvim_lsp_signature_help' },
-                { name = 'path' },
-                { name = 'spell' },
-                { name = 'vsnip' },
-            }),
-            snippet = {
-                expand = function(args)
-                    luasnip.lsp_expand(args.body)
-                end,
-            },
-            mapping = cmp.mapping.preset.insert({
-                ['<C-n>'] = cmp.mapping.select_next_item(),
-                ['<C-p>'] = cmp.mapping.select_prev_item(),
-                ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete({}),
-                ['<CR>'] = cmp.mapping.confirm({
-                    behavior = cmp.ConfirmBehavior.Replace,
-                    select = true,
-                }),
-                ['<Tab>'] = function(fallback)
-                    if not cmp.select_next_item() then
-                        if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end
-                end,
-                ['<S-Tab>'] = function(fallback)
-                    if not cmp.select_prev_item() then
-                        if vim.bo.buftype ~= 'prompt' and has_words_before() then
-                            cmp.complete()
-                        else
-                            fallback()
-                        end
-                    end
-                end,
-                ['<M-Down>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_next_item()
-                    elseif luasnip.expand_or_locally_jumpable() then
-                        luasnip.expand_or_jump()
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-                ['<M-Up>'] = cmp.mapping(function(fallback)
-                    if cmp.visible() then
-                        cmp.select_prev_item()
-                    elseif luasnip.locally_jumpable(-1) then
-                        luasnip.jump(-1)
-                    else
-                        fallback()
-                    end
-                end, { 'i', 's' }),
-            }),
-        })
-    end,
+		cmp.setup({
+			sources = cmp.config.sources({
+				-- { name = 'clojure' },
+				-- { name = 'conjure' },
+				{ name = "buffer" },
+				{ name = "luasnip" },
+				{ name = "nvim_lsp" },
+				{ name = "nvim_lsp_signature_help" },
+				{ name = "path" },
+				{ name = "spell" },
+				{ name = "vsnip" },
+			}),
+			snippet = {
+				expand = function(args)
+					luasnip.lsp_expand(args.body)
+				end,
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<C-n>"] = cmp.mapping.select_next_item(),
+				["<C-p>"] = cmp.mapping.select_prev_item(),
+				["<C-d>"] = cmp.mapping.scroll_docs(-4),
+				["<C-f>"] = cmp.mapping.scroll_docs(4),
+				["<C-Space>"] = cmp.mapping.complete({}),
+				["<CR>"] = cmp.mapping.confirm({
+					behavior = cmp.ConfirmBehavior.Replace,
+					select = true,
+				}),
+				["<Tab>"] = function(fallback)
+					if not cmp.select_next_item() then
+						if vim.bo.buftype ~= "prompt" and has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end
+				end,
+				["<S-Tab>"] = function(fallback)
+					if not cmp.select_prev_item() then
+						if vim.bo.buftype ~= "prompt" and has_words_before() then
+							cmp.complete()
+						else
+							fallback()
+						end
+					end
+				end,
+				["<M-Down>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_locally_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<M-Up>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif luasnip.locally_jumpable(-1) then
+						luasnip.jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+			}),
+		})
+	end,
 }
--- lua/plugins/conjure.lua
+++ lua/plugins/conjure.lua
@@ -3,8 +3,11 @@ return {
 	"Olical/conjure",
 
 	-- Structural editing, optional
-	"guns/vim-sexp",
-	"tpope/vim-sexp-mappings-for-regular-people",
-	"tpope/vim-repeat",
-	"tpope/vim-surround",
+	dependencies = {
+		{ "guns/vim-sexp" },
+		{ "tpope/vim-sexp-mappings-for-regular-people" },
+		{ "tpope/vim-repeat" },
+		{ "tpope/vim-surround" },
+		{ "PaterJason/cmp-conjure" },
+	},
 }
